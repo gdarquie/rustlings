be27b04c0ee6322d2b3bf35b92389984838b975d
gdarquie <gaetan.darquie@gmail.com>
2023-02-19 00:06:44 +0100

    If, primitives, quiz1 and vecs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+// I AM DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut appleCost = 2;
+    if (quantity > 40) {
+        appleCost = 1
+    }
+    return quantity * appleCost;
+}
 
 // Don't modify this function!
 #[test]
