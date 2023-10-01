baac02551139a25d404453b62e7fb8e7d9e8b187
gdarquie <gaetan.darquie@beta.gouv.fr>
2023-10-01 17:07:00 +0200

    Add hashmap3 solution

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -35,11 +35,56 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        let mut team1 = Team {
+            name: team_1_name.clone(),
+            goals_scored: team_1_score.clone(),
+            goals_conceded: team_2_score.clone(),
+        };
+
+        let mut temp_team_1 = scores.get(&team_1_name);
+
+        if temp_team_1.is_none() {
+            scores.insert(team_1_name, team1);
+        } else {
+            let temp2 = temp_team_1.unwrap();
+
+            let temp3 = Team {
+                name: team_1_name.clone(),
+                goals_scored: temp2.goals_scored.clone() + team_1_score.clone(),
+                goals_conceded: temp2.goals_conceded.clone() + team_2_score.clone(),
+            };
+            scores.insert(team_1_name, temp3);
+        }
+
+        let mut team2 = Team {
+            name: team_2_name.clone(),
+            goals_scored: team_2_score.clone(),
+            goals_conceded: team_1_score.clone(),
+        };
+
+        let mut temp_team_2 = scores.get(&team_2_name);
+
+        if temp_team_2.is_none() {
+            scores.insert(team_2_name, team2);
+        } else {
+            let temp2 = temp_team_2.unwrap();
+
+            let temp3 = Team {
+                name: team_2_name.clone(),
+                goals_scored: temp2.goals_scored.clone() + team_2_score.clone(),
+                goals_conceded: temp2.goals_conceded.clone() + team_1_score.clone(),
+            };
+            scores.insert(team_2_name, temp3);
+        }
+
+
     }
     scores
 }
